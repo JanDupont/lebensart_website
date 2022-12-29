e8ba38e5027694297393bac9a0a7384c1533cd7d
JanDupont <80339445+JanDupont@users.noreply.github.com>
2022-12-29 20:23:29 +0100

    info banner: öffnungszeiten bis silvester

--- src/components/LandingInfoBanner.vue
+++ src/components/LandingInfoBanner.vue
@@ -5,7 +5,14 @@
 			src="/assets/img/LebensArt_Dupont_Hermeskeil_infoImg_Silvester.jpg"
 			cover
 		></v-img>
-		<div class="ma-2 text-h6 text-white">Donnerstag Nachmittag geschlossen! (29.12.2022)</div>
+		<div class="ma-2 text-h6 text-white">
+			Öffnungszeiten bis Silvester:
+			<br />
+			<br />
+			<strong>Fr</strong> (30.12): 10-11:30 & 15-19:00
+			<br />
+			<strong>Sa</strong> (31.12): 10-12:00 Uhr
+		</div>
 	</v-card>
 </template>
 
