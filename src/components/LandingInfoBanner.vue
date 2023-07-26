21b56fcbb470c3e4f0009ea32a485c4310845014
JanDupont <80339445+JanDupont@users.noreply.github.com>
2023-07-26 14:30:20 +0200

    neue öffnungszeiten overall

--- src/components/LandingInfoBanner.vue
+++ src/components/LandingInfoBanner.vue
@@ -3,24 +3,7 @@
 		<!-- cover -->
 		<!-- <v-img class="infoBannerCardImg" src="/assets/img/LebensArt_Dupont_Hermeskeil_Frühling.jpg"></v-img> -->
 		<div class="ma-2 text-h6 text-white">
-			<!-- {{ t("InfoBanner.text1") }}
-			<br />
-			{{ t("InfoBanner.text2") }} -->
-			folgende <span style="color: rgb(204, 16, 16)">Öffnungszeiten</span> vom 25. Juli - 29. Juli:
-			<br />
-			<v-row v-for="tag in öffnungszeiten" class="ma-0 mt-1">
-				<v-col cols="12" md="2" class="pa-0" style="color: rgb(204, 16, 16)">
-					{{ tag.tag }}
-				</v-col>
-				<v-col cols="12" md="6" class="pa-0">
-					{{ tag.öffnungszeiten }}
-				</v-col>
-			</v-row>
-			oder Tel: 06503/8933
-			<br />
-			❤️ Dank für euer Verständnis
-			<br />
-			Silvia Dupont
+			am <span style="color: rgb(204, 16, 16)">Donnerstag</span>, den 27.07.2023 nur bis <span style="color: rgb(204, 16, 16)">18 Uhr</span> geöffnet.
 		</div>
 	</v-card>
 </template>
@@ -41,7 +24,7 @@ const öffnungszeiten = [
 	},
 	{
 		tag: "Do:",
-		öffnungszeiten: "10-12 Uhr & 17-19 Uhr",
+		öffnungszeiten: "10-12 Uhr & 17-18 Uhr",
 	},
 	{
 		tag: "Fr:",
